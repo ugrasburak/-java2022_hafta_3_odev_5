72ac78c7a819f5a2f9f672878b5160e96cbffc95
ugrasburak <114237134+ugrasburak@users.noreply.github.com>
2022-10-19 14:37:09 +0300

    Add files via upload

---
+++ SQLQuery2.sql
@@ -0,0 +1,27 @@
+﻿﻿--Select
+Select ContactName Adi,CompanyName Sirketadi,City Sehir from Customers
+
+Select*from Customers where City='Berlin'
+
+--case insensitive
+select*from Products where categoryId=1 or categoryId=3
+
+select*from Products where categoryId=1 and UnitPrice>10
+
+select*from Products where categoryId=1 order by UnitPrice desc   --ascending  -- descending
+
+select count(*) from Products where CategoryID=2
+
+select categoryId,count(*) from products where UnitPrice>20 group by CategoryId having count(*)<10
+
+
+select Products.ProductID,Products.ProductName,Products.UnitPrice,Categories.CategoryName
+from Products inner join Categories
+on Products.CategoryID=Categories.CategoryID
+where Products.UnitPrice>10
+
+--DTO Data Transformation object
+
+Select*from Products p inner join [Order Details] od on p.ProductID=od.ProductID inner join Orders o on o.OrderID=od.orderID
+
+Select*from Customers c left join Orders o on c.CustomerID=o.CustomerID where o.CustomerID is null
